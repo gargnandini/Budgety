62501e3772223998817515fdb4af4fb8cdfe3c17
gargnandini <nandinigarh1204@gmail.com>
2020-08-01 14:43:26 +0530

    deleting the item from UI!

--- app.js
+++ app.js
@@ -154,6 +154,11 @@ var UIController = (function() {
 
         },
 
+        deleteListItem: function(selectorID) {
+            var el = document.getElementById(selectorID);
+            el.parentNode.removeChild(el);
+        },
+
         clearFields: function() {
             var fields, fieldsArr;
             fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
@@ -250,8 +255,10 @@ var controller = (function(budgetCtrl, UICtrl) {
             budgetController.deleteItem(type, ID);
 
             //2 delete the item from the UI
+            UICtrl.deleteListItem(itemID);
 
             //3 update and show new totals(budget)
+            updateBudget();
         }
     };
 
